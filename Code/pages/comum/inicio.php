f61df148d1c0d17767e160f32717447dd75c002b
Gonçalo Martins <martinsffg@gmail.com>
2020-12-27 18:37:14 +0000

    inicio com publicidade e logins

--- Code/pages/comum/inicio.php
+++ Code/pages/comum/inicio.php
@@ -33,10 +33,98 @@
         
     </div>
 
+    <div class="main-container">
+            <h3>Publicidade</h3>
+            <!-- Slideshow container -->
+            <div class="slideshow-container">
+    
+                <div class="mySlides fade">
+                <a href="socio.php">
+                    <img src="../../images/galeria1.png">
+                </a>
+                </div>
+            
+                <div class="mySlides fade">
+                <a href="loja.php">
+                    <img src="../../images/galeria2.png">
+                </a>
+                </div>
+            
+                <div class="mySlides fade">
+                <a href="membros.php">
+                    <img src="../../images/galeria3.png">
+                </a>
+                </div>
+            
+                <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+                <a class="next" onclick="plusSlides(1)">&#10095;</a>
+            </div>
+            <br>
+            
+            <!-- The dots/circles -->
+            <div style="text-align:center">
+                <span class="dot" onclick="currentSlide(1)"></span>
+                <span class="dot" onclick="currentSlide(2)"></span>
+                <span class="dot" onclick="currentSlide(3)"></span>
+            </div>
+        </div>
+
+    </main>
+
     <?php 
         include '../../includes/footer.html';
         include '../../includes/modal_login.php';
      ?>
+
+<script>
+var slideIndex = 0;
+showSlides();
+var slides,dots;
+
+function showSlides() {
+    var i;
+    slides = document.getElementsByClassName("mySlides");
+    dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex> slides.length) {slideIndex = 1}    
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+    setTimeout(showSlides, 4000); // Change image every 4 seconds
+}
+
+function plusSlides(position) {
+    slideIndex +=position;
+    if (slideIndex> slides.length) {slideIndex = 1}
+    else if(slideIndex<1){slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";  
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+}
+
+function currentSlide(index) {
+    if (index> slides.length) {index = 1}
+    else if(index<1){index = slides.length}
+    for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";  
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[index-1].style.display = "block";  
+    dots[index-1].className += " active";
+}
+</script>
 
 </body>
 </html>
